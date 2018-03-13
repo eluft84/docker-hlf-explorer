dc1941e5e190e34904948aa6e751bd5e41ad50ff
Eugen Luft <eugenjunluft@gmail.com>
2018-03-13 11:30:42 +0100

    Add Working example

---
+++ start-example-network.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# This script will setup a simple FabCar Network with running Couchdb and Blockchain Explorer
+
+set -ev
+
+# don't rewrite paths for Windows Git Bash users 
+export MSYS_NO_PATHCONV=1
+export COMPOSE_PROJECT_NAME=net
+
+
+docker-compose -f example/docker-compose.yaml down
+docker-compose -f example/docker-compose.yaml up -d
+
+
+# wait for Hyperledger Fabric to start 
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number> 
+
+export FABRIC_START_TIMEOUT=10
+sleep ${FABRIC_START_TIMEOUT}
+
+# Create the channel
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+# Join peer0.org1.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
+
+
+#sleep 5
+
+# Now install, instantiate chaincode
+# and prime the ledger with our 10 cars
+
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n fabcar -v 1.0 -p github.com/fabcar
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n fabcar -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+sleep 5
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabcar -c '{"function":"initLedger","Args":[""]}'
+
+set +ev
+
+printf "\nTotal setup execution time : $(($(date +%s) - starttime)) secs ...\n\n\n"
+printf "Start by installing required packages run 'npm install'\n"
+printf "Then run 'node enrollAdmin.js', then 'node registerUser'\n\n"
+printf "The 'node invoke.js' will fail until it has been updated with valid arguments\n"
+printf "The 'node query.js' may be run at anytime once the user has been registered\n\n"
+printf "Open localhost:8080 to see the Blockchain Explorer Web-Interface\n\n"
